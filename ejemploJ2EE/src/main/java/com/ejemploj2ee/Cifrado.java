374dcf908d25b26aec1fb5e9ee755f1d0e7de502
pepe navarro <oscar.novillo@gmail.com>
2016-01-08 00:21:17 +0100

    Merge origin/master
    Conflicts:
    	swingImagenes/src/main/java/com/swingimagenes/model/Imagen.java

---
+++ ejemploJ2EE/src/main/java/com/ejemploj2ee/Cifrado.java
@@ -0,0 +1,126 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ejemploj2ee;
+
+import com.ejemploj2ee.model.Juego;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.MessageDigest;
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.commons.codec.binary.Base64;
+
+/**
+ *
+ * @author oscar
+ */
+@WebServlet(name = "Cifrado", urlPatterns = {"/Cifrado"})
+public class Cifrado extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+       
+         Juego j = new Juego("gowrrrr", 1);
+         try {
+                ObjectMapper mapper = new ObjectMapper();
+                String obj = mapper.writeValueAsString(j);
+                byte [] bytes = cifra(obj);
+                String mandar = new String(Base64.encodeBase64(bytes));
+                response.getWriter().print(mandar);
+            } catch (Exception ex) {
+
+            }
+         
+         
+        
+    }
+    
+    public byte[] cifra(String sinCifrar) throws Exception {
+        final byte[] bytes = sinCifrar.getBytes("UTF-8");
+        final Cipher aes = obtieneCipher(true);
+        final byte[] cifrado = aes.doFinal(bytes);
+        return cifrado;
+    }
+
+    public String descifra(byte[] cifrado) throws Exception {
+        final Cipher aes = obtieneCipher(false);
+        final byte[] bytes = aes.doFinal(cifrado);
+        final String sinCifrar = new String(bytes, "UTF-8");
+        return sinCifrar;
+    }
+
+    private Cipher obtieneCipher(boolean paraCifrar) throws Exception {
+        final String frase = "FraseLargaConDiferentesLetrasNumerosYCaracteresEspeciales_áÁéÉíÍóÓúÚüÜñÑ1234567890!#%$&()=%_NO_USAR_ESTA_FRASE!_";
+        final MessageDigest digest = MessageDigest.getInstance("SHA-1");
+        digest.update("clave".getBytes("UTF-8"));
+        final SecretKeySpec key = new SecretKeySpec(digest.digest(), 0, 16, "AES");
+
+        final Cipher aes = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        if (paraCifrar) {
+            aes.init(Cipher.ENCRYPT_MODE, key);
+        } else {
+            aes.init(Cipher.DECRYPT_MODE, key);
+        }
+
+        return aes;
+    }
+
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
