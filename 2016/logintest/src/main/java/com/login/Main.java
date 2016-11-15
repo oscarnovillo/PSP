5e3c02c3733df42aeaded1090777dce2622bed87
pepe navarro <oscar.novillo@gmail.com>
2016-11-15 16:44:07 +0100

    Merge origin/master
    Conflicts:
    	ejemploTablas/src/main/java/com/ejemplotablas/JFrameDatos.java

---
+++ 2016/logintest/src/main/java/com/login/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.login;
+
+/**
+ *
+ * @author oscar
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        //conexion con BD y meter usuario.
+        
+        
+        
+        
+    }
+    
+}
