d868897593207c96386164b0a5a83aa4e6024997
oscar <oscar@2DAM-PROFE.IES.QUEVEDO-AD>
2017-02-23 09:17:00 +0100

    Merge origin/master
    Conflicts:
    	clienteWeb/server1024.privada
    	clienteWeb/server1024.publica
    	clienteWeb/server1024.secreta
    	clienteWeb/src/main/java/com/clienteweb/CifrarRSAFicheros.java

---
+++ 2016/ChatClient/src/main/java/com/datoshttp/OrdenWS.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.datoshttp;
+
+/**
+ *
+ * @author oscar
+ */
+public class OrdenWS {
+    
+    private String orden;
+
+    public String getOrden() {
+        return orden;
+    }
+
+    public void setOrden(String orden) {
+        this.orden = orden;
+    }
+    
+    
+    
+    
+}
---
+++ 2016/chatServer/src/main/java/model/Juego.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author oscar
+ */
+public class Juego {
+    
+    private int id;
+    private String nombre;
+    private Date fecha;
+
+    public Juego(int id, String nombre,Date fecha) {
+        this.id = id;
+        this.nombre = nombre;
+        this.fecha = fecha;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+    
+}
